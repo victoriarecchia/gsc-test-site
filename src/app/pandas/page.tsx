f9dbb898d0b7186e3d5c1082fda7df2a060637f2
Victoria Recchia <victoriarecchia98@gmail.com>
2026-08-25 12:34:13 +0000

    a

---
+++ src/app/pandas/page.tsx
@@ -0,0 +1,12 @@
+export default function PandasPage() {
+  return (
+    <main>
+      <h1>Pandas</h1>
+
+      <p>
+        Pandas is a Python library used for
+        data manipulation and analysis.
+      </p>
+    </main>
+  );
+}
