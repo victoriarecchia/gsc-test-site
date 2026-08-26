d555d8d020029ac69d25040ca030812337999aeb
Victoria Recchia <victoriarecchia98@gmail.com>
2026-08-26 08:38:00 +0000

    add metadata for SEO and analytics in layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,22 @@
 import "./globals.css";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Victoria's SEO Analytics Lab",
+  description: "Learning Python, PostgreSQL and Data Analytics",
+  verification: {
+    google: "h1J1mEh8_8zsEygFgKDoLi9L_v-k9CO6S_ObAfCsJMI",
+  },
+};
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
     <html lang="en">
-      <body>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
